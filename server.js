769549e0cd7c48a30be0d4a7204cfdbbd82e8d27
Aaron Baker <aaronbakeremail@gmail.com>
2016-05-03 12:07:05 +0100

    Began implementing mongodb database for logins - added mongoose, passport and express-session to dependencies as well as jade. Added db.js database config file.

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation
- * purposes only. Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
@@ -18,8 +6,26 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+//path to comments file - temporary, to be replaced by database
 var COMMENTS_FILE = path.join(__dirname, 'messages.json');
 
+//database config
+var dbConfig = require('./db.js');
+var mongoose = require('mongoose');
+mongoose.connect(dbConfig.url);
+
+
+
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+app.use(expressSession({secret: 'secret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//set port for app
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
