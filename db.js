c4fa86eab5d353c87c54f005f32517cba51bbe4a
Aaron Baker <aaronbakeremail@gmail.com>
2016-05-17 16:30:51 +0100

    changed db info to cloud hosted database on mlab

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'url' : 'mongodb://localhost/tellyprompt'
+  //'url' : 'mongodb://localhost/tellyprompt'
+    'url' : 'mongodb://root:root@ds023902.mlab.com:23902/tellyprompt'
 }
